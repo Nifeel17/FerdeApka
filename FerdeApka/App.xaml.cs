2a175ec4dcf4a56e0b8fb711a8c0fcf04aec3694
Nifeel17 <Nifeel17@gmail.com>
2024-01-21 18:52:59 +0100

    Dodaj pliki projektów.

---
+++ FerdeApka/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FerdeApka
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
