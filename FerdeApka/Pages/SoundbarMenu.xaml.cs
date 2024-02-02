c6f9c919878f701d185405f7f63592da7af0db28
Nifeel17 <Nifeel17@gmail.com>
2024-02-02 14:27:38 +0100

    Refaktoryzacja SoundbarMenu

--- FerdeApka/Pages/SoundbarMenu.xaml.cs
+++ FerdeApka/Pages/SoundbarMenu.xaml.cs
@@ -1,99 +1,60 @@
+using CommunityToolkit.Maui.Views;
+
 namespace FerdeApka.Pages;
 
 public partial class SoundbarMenu : TabbedPage
 {
-	public SoundbarMenu()
+    
+    Dictionary<int, MediaElement> SlownikMediaElement;
+    Dictionary<int, Button> SlownikPrzyciskow;
+	
+    public SoundbarMenu()
 	{
 		InitializeComponent();
+        SlownikMediaElement = new Dictionary<int, MediaElement>()
+        {
+            { 1, DzwiekCoWTymSmiesznego },
+            { 2, DzwiekFakJu },
+            { 3, DzwiekGoloneczkiBysChcial },
+            { 4, DzwiekJakNieMogeToPrzezNoge },
+            { 5, DzwiekWstydzSie },
+            { 6, DzwiekNiePekaj },
+
+        };
+
+        SlownikPrzyciskow = new Dictionary<int, Button>()
+        {
+            { 1, SoundbarButtonDzwiekCoWTymSmiesznego },
+            { 2, SoundbarButtonDzwiekFakJu },
+            { 3, SoundbarButtonDzwiekGoloneczkiBysChcial },
+            { 4, SoundbarButtonDzwiekJakNieMogeToPrzezNoge },
+            { 5, SoundbarButtonDzwiekWstydzSie},
+            { 6, SoundbarButtonDzwiekNiePekaj }
+        };
 	}
 
-    private void SoundbarDzwiekCoWTymSmisznegoZagraj(object sender, EventArgs e)
+    private void ZagrajDzwiek(object sender, EventArgs e)
     {
-        if (DzwiekCoWTymSmiesznego.CurrentState == CommunityToolkit.Maui.Core.Primitives.MediaElementState.Playing)
+        int index = 1;
+        foreach(var x in SlownikPrzyciskow)
         {
-            DzwiekCoWTymSmiesznego.Stop();
-            DzwiekCoWTymSmiesznego.SeekTo(TimeSpan.Zero);
-            DzwiekCoWTymSmiesznego.Play();
-        }
-        else if (DzwiekCoWTymSmiesznego.CurrentState == CommunityToolkit.Maui.Core.Primitives.MediaElementState.Paused || DzwiekCoWTymSmiesznego.CurrentState == CommunityToolkit.Maui.Core.Primitives.MediaElementState.Stopped)
-        {
-            DzwiekCoWTymSmiesznego.SeekTo(TimeSpan.Zero);
-            DzwiekCoWTymSmiesznego.Play();
-        }
-    }
-
-    private void SoundbarDzwiekFakJuZagraj(object sender, EventArgs e)
-    {
-        if (DzwiekFakJu.CurrentState == CommunityToolkit.Maui.Core.Primitives.MediaElementState.Playing)
-        {
-            DzwiekFakJu.Stop();
-            DzwiekFakJu.SeekTo(TimeSpan.Zero);
-            DzwiekFakJu.Play();
-        }
-        else if(DzwiekFakJu.CurrentState == CommunityToolkit.Maui.Core.Primitives.MediaElementState.Paused || DzwiekFakJu.CurrentState == CommunityToolkit.Maui.Core.Primitives.MediaElementState.Stopped)
-        {
-            DzwiekFakJu.SeekTo(TimeSpan.Zero);
-            DzwiekFakJu.Play();
-        }
-    }
-
-    private void SoundbarDzwiekCoWTymSmiesznegoZagraj(object sender, EventArgs e)
-    {
-        if (DzwiekGoloneczkiBysChcial.CurrentState == CommunityToolkit.Maui.Core.Primitives.MediaElementState.Playing)
-        {
-            DzwiekGoloneczkiBysChcial.Stop();
-            DzwiekGoloneczkiBysChcial.SeekTo(TimeSpan.Zero);
-            DzwiekGoloneczkiBysChcial.Play();
-        }
-        else if (DzwiekGoloneczkiBysChcial.CurrentState == CommunityToolkit.Maui.Core.Primitives.MediaElementState.Paused || DzwiekGoloneczkiBysChcial.CurrentState == CommunityToolkit.Maui.Core.Primitives.MediaElementState.Stopped)
-        {
-            DzwiekGoloneczkiBysChcial.SeekTo(TimeSpan.Zero);
-            DzwiekGoloneczkiBysChcial.Play();
-        }
-    }
-
-    private void SoundbarDzwiekJakNieMogeToPrzezNogeZagraj(object sender, EventArgs e)
-    {
-        if (DzwiekJakNieMogeToPrzezNoge.CurrentState == CommunityToolkit.Maui.Core.Primitives.MediaElementState.Playing)
-        {
-            DzwiekJakNieMogeToPrzezNoge.Stop();
-            DzwiekJakNieMogeToPrzezNoge.SeekTo(TimeSpan.Zero);
-            DzwiekJakNieMogeToPrzezNoge.Play();
-        }
-        else if (DzwiekJakNieMogeToPrzezNoge.CurrentState == CommunityToolkit.Maui.Core.Primitives.MediaElementState.Paused || DzwiekJakNieMogeToPrzezNoge.CurrentState == CommunityToolkit.Maui.Core.Primitives.MediaElementState.Stopped)
-        {
-            DzwiekJakNieMogeToPrzezNoge.SeekTo(TimeSpan.Zero);
-            DzwiekJakNieMogeToPrzezNoge.Play();
-        }
-    }
-
-    private void SoundbarDzwiekWstydzSieZagraj(object sender, EventArgs e)
-    {
-        if (DzwiekWstydzSie.CurrentState == CommunityToolkit.Maui.Core.Primitives.MediaElementState.Playing)
-        {
-            DzwiekWstydzSie.Stop();
-            DzwiekWstydzSie.SeekTo(TimeSpan.Zero);
-            DzwiekWstydzSie.Play();
-        }
-        else if (DzwiekWstydzSie.CurrentState == CommunityToolkit.Maui.Core.Primitives.MediaElementState.Paused || DzwiekWstydzSie.CurrentState == CommunityToolkit.Maui.Core.Primitives.MediaElementState.Stopped)
-        {
-            DzwiekWstydzSie.SeekTo(TimeSpan.Zero);
-            DzwiekWstydzSie.Play();
-        }
-    }
-
-    private void SoundbarDzwiekNiePekajZagraj(object sender, EventArgs e)
-    {
-        if (DzwiekNiePekaj.CurrentState == CommunityToolkit.Maui.Core.Primitives.MediaElementState.Playing)
-        {
-            DzwiekNiePekaj.Stop();
-            DzwiekNiePekaj.SeekTo(TimeSpan.Zero);
-            DzwiekNiePekaj.Play();
-        }
-        else if (DzwiekNiePekaj.CurrentState == CommunityToolkit.Maui.Core.Primitives.MediaElementState.Paused || DzwiekNiePekaj.CurrentState == CommunityToolkit.Maui.Core.Primitives.MediaElementState.Stopped)
-        {
-            DzwiekNiePekaj.SeekTo(TimeSpan.Zero);
-            DzwiekNiePekaj.Play();
+            if(x.Value.ClassId == ((Button)sender).ClassId)
+            {
+                SlownikMediaElement[index].Play();
+                if (SlownikMediaElement[index].CurrentState == CommunityToolkit.Maui.Core.Primitives.MediaElementState.Playing)
+                {
+                    SlownikMediaElement[index].Stop();
+                    SlownikMediaElement[index].SeekTo(TimeSpan.Zero);
+                    SlownikMediaElement[index].Play();
+                }
+                else if (DzwiekFakJu.CurrentState == CommunityToolkit.Maui.Core.Primitives.MediaElementState.Paused || SlownikMediaElement[index].CurrentState == CommunityToolkit.Maui.Core.Primitives.MediaElementState.Stopped)
+                {
+                    SlownikMediaElement[index].SeekTo(TimeSpan.Zero);
+                    SlownikMediaElement[index].Play();
+                }
+                break;
+            }
+            index++;
         }
     }
 }
