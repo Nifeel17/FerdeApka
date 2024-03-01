07e5526f6b67a273f0538788ca154d3b9b3cc7f5
Nifeel17 <Nifeel17@gmail.com>
2024-03-01 19:29:13 +0100

    Dokończenie prac nad karuzelą na stronie głównej, pozbycie sie starego wyglądu

--- FerdeApka/Pages/StronaGlowna.xaml.cs
+++ FerdeApka/Pages/StronaGlowna.xaml.cs
@@ -15,14 +15,14 @@ public partial class StronaGlowna : ContentPage
         wyswietlanieFerdePunktow.Text = $"FerdePunkty: {Preferences.Default.Get("FerdePunkty",0)}";
         List<ModelKaruzela> Karuzelka = new List<ModelKaruzela>()
         {
-            new ModelKaruzela() { Nazwa="Soundbar", Opis="Najlepsze dŸwiêki z serialu!", Strona=new SoundbarMenu(), Obrazek="marianpazdzioch.png"},
-            new ModelKaruzela() { Nazwa="Quizy", Opis="Czy odpowiesz poprrawnie na wszystkie?", Strona=new StronaWybieraniaQuizow(), Obrazek="dotnet_bot.png"},
-            new ModelKaruzela() { Nazwa="Przegladaj memy", Opis="Dobre meme, powa¿nie!", Strona=new StronaMemow(), Obrazek="strzalka.png"},
-            new ModelKaruzela() { Nazwa="Clicker", Opis="Poklikaj sobie", Strona=new StronaClicker(), Obrazek="logoferdek.png"},
-            new ModelKaruzela() { Nazwa="Kasyno", Opis="Przewal ca³y swój maj¹tek (FerdePunktów) ju¿ dzisiaj!", Strona=new StronaKasyno(), Obrazek="strzalka.png"},
-            new ModelKaruzela() { Nazwa="Daily", Opis="Dzienny cytat i mem", Strona=new StronaDaily(), Obrazek="strzalka.png"}
+            new ModelKaruzela() { Nazwa="Soundbar", Opis="Najlepsze dŸwiêki z serialu!", Strona=new SoundbarMenu(), Obrazek="ferdekmuzyk.png"},
+            new ModelKaruzela() { Nazwa="Quizy", Opis="Czy odpowiesz poprrawnie na wszystkie?", Strona=new StronaWybieraniaQuizow(), Obrazek="ferdekrece.png"},
+            new ModelKaruzela() { Nazwa="Przegladaj memy", Opis="Dobre meme, powa¿nie!", Strona=new StronaMemow(), Obrazek="ferdeklampa.png"},
+            new ModelKaruzela() { Nazwa="Clicker", Opis="Poklikaj sobie", Strona=new StronaClicker(), Obrazek="ferdekpiwko.png"},
+            new ModelKaruzela() { Nazwa="Kasyno", Opis="Przewal ca³y swój maj¹tek (FerdePunktów) ju¿ dzisiaj!", Strona=new StronaKasyno(), Obrazek="ferdekkasyno.png"},
+            new ModelKaruzela() { Nazwa="Daily", Opis="Dzienny cytat i mem", Strona=new StronaDaily(), Obrazek="ferdekgangsta.png"}
         };
-        karuz.ItemsSource=Karuzelka;
+        karuzela.ItemsSource=Karuzelka;
     }
 
     public StronaGlowna(string doKad)
@@ -48,29 +48,13 @@ public partial class StronaGlowna : ContentPage
         await Navigation.PushAsync(new StronaWybieraniaQuizow());
     }
 
-    private async void PrzejdzDoStronySoundbarow(object sender, EventArgs e)
-    {
-        await Navigation.PushAsync(new SoundbarMenu());
-    }
-
+    
     private void PokazPopupOAplikacji(object sender, EventArgs e)
     {
         this.ShowPopup(new PopupInfoOAplikacji());
     }
 
-    private void LogoFerdkaKlikniete(object sender, EventArgs e)
-    {
-        if (DzwiekNaGlownymEkranie.CurrentState == CommunityToolkit.Maui.Core.Primitives.MediaElementState.Paused || DzwiekNaGlownymEkranie.CurrentState == CommunityToolkit.Maui.Core.Primitives.MediaElementState.Stopped)
-        {
-            DzwiekNaGlownymEkranie.SeekTo(TimeSpan.Zero);
-            DzwiekNaGlownymEkranie.Play();
-        }
-    }
-
-    private async void PrzejdzDoStronyQuizow(object sender, EventArgs e)
-    {
-        await Navigation.PushAsync(new StronaWybieraniaQuizow());
-    }
+  
 
     private async void PrzejdzDoStronyMemow(object sender, EventArgs e)
     {
@@ -101,21 +85,11 @@ public partial class StronaGlowna : ContentPage
         }
     }
 
-    private async void PrzejdzDoStronyClicker(object sender, EventArgs e)
-    {
-        await Navigation.PushAsync(new StronaClicker());
-    }
-
-    private async void PrzejdzDoStronyKasyno(object sender, EventArgs e)
-    {
-        await Navigation.PushAsync(new StronaKasyno());
-    }
-
     private void NacisnietoFerdePunkty(object sender, TappedEventArgs e)
     {
         this.ShowPopup(new PopupFerdePunkty());
     }
-
+    //potem moge wywalic
     private void DodajFerdePunkty(object sender, EventArgs e)
     {
         Preferences.Default.Set("FerdePunkty",Preferences.Default.Get("FerdePunkty",0)+100);
@@ -131,13 +105,29 @@ public partial class StronaGlowna : ContentPage
     {
         Preferences.Default.Set("OdblokowaneSoundbary","00000000000000");
     }
-
+    //az tutaj
     private async void KliknietaKaruzela(object sender, TappedEventArgs e)
     {
         var item = e.Parameter as ModelKaruzela;
         if (item != null)
         {
-            await Navigation.PushAsync(item.Strona);
+            if(item.Nazwa=="Przegladaj memy" || item.Nazwa == "Daily")
+            {
+                NetworkAccess czyJestNet = Connectivity.Current.NetworkAccess;
+
+                if (czyJestNet == NetworkAccess.Internet)
+                {
+                    await Navigation.PushAsync(item.Strona);
+                }
+                else
+                {
+                    this.ShowPopup(new PopupPotrzebnyInternet());
+                }
+            }
+            else
+            {
+                await Navigation.PushAsync(item.Strona);
+            }
         }
     }
 
