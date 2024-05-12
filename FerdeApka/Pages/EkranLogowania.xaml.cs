8d666fc94e5eee8dcc335ca3236f8e106eb6632e
Nifeel17 <Nifeel17@gmail.com>
2024-05-12 19:52:45 +0200

    Dodanie logowania do aplikacji

--- FerdeApka/Pages/EkranLogowania.xaml.cs
+++ FerdeApka/Pages/EkranLogowania.xaml.cs
@@ -9,7 +9,6 @@ public partial class EkranLogowania : ContentPage
 	public EkranLogowania()
 	{
 		InitializeComponent();
-        Preferences.Default.Set("FerdePunkty",50);//tymczasowe, do usuniecia
 	}
 
     private async void PowrotDoWstepu(object sender, EventArgs e)
@@ -24,7 +23,22 @@ public partial class EkranLogowania : ContentPage
 
     private async void ZalogowanieUzytkownika(object sender, EventArgs e)
     {
-        //utworzyc logike do logowania, dluzsze zadanie, tymczasowo zostawione na potem
-        Application.Current.MainPage = new NavigationPage(new StronaGlowna());
+        if (email_uzytkownika_entry.Text == null || email_uzytkownika_entry.Text == "" || haslo_uzytkownika_entry==null || haslo_uzytkownika_entry.Text=="")
+            return;
+
+        HttpClient klient = new HttpClient();
+
+        var dictToPost = new Dictionary<string, string> { { "login", email_uzytkownika_entry.Text }, { "haslo", haslo_uzytkownika_entry.Text } };
+
+        var dataToPost = new FormUrlEncodedContent(dictToPost);
+
+        var checkData = await klient.PostAsync("http://localhost/FerdeApi/", dataToPost);//tak, localhost. Gdyby by³ to projekt wart rozwiajania to szuka³bym hostingu, ale to projekt na którym uczy³em sie podstaw maui. Poprostu dodam logowanie i uciekam
+
+        var content = await checkData.Content.ReadAsStringAsync();
+
+        if (string.IsNullOrEmpty(content))
+            return;
+        else
+            Application.Current.MainPage = new NavigationPage(new StronaGlowna());//sprawdzam tylko czy uzytkownik istnieje, jego nazwa pozniej nie ma znaczenia
     }
 }
--- FerdeApka/Pages/EkranWstepny.xaml.cs
+++ FerdeApka/Pages/EkranWstepny.xaml.cs
@@ -12,11 +12,19 @@ public partial class EkranWstepny : ContentPage
 
     private async void LoginWithAccount(object sender, EventArgs e)
     {
-		await Navigation.PushAsync(new EkranLogowania());
+        if (Connectivity.Current.NetworkAccess == NetworkAccess.Internet)
+            await Navigation.PushAsync(new EkranLogowania());
+        else
+            this.ShowPopup(new PopupPotrzebnyInternet());
     }
 
     private void LoginWithFacebook(object sender, EventArgs e)
     {
         this.ShowPopup(new PopupLogowanieZFacebooka());
+    }
+
+    private void PassedLogin(object sender, EventArgs e)
+    {
+        Application.Current.MainPage = new NavigationPage(new StronaGlowna());
     }
 }
